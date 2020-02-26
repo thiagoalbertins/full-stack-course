53333c730dba551d89304617a75c0c5513a16513
thiagoalbertins <thiagoalbertins@gmail.com>
2020-02-26 20:25:13 -0300

    User register

--- modulo02/src/database/migrations/20200226164714-create-users.js
+++ modulo02/src/database/migrations/20200226164714-create-users.js
@@ -22,7 +22,7 @@ module.exports = {
       },
       provider: {
         type: Sequelize.BOOLEAN,
-        deafualtValue: false,
+        defaultValue: false,
         allowNull: false,
       },
       created_at: {
